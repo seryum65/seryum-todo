ef0723bfeb100e716c642010ff8a9fac56d230d1
seryum65 <cloudengineer.yusuf@gmail.com>
2023-06-07 09:12:02 +0000

    updt

--- todo_project/main.tf
+++ todo_project/main.tf
@@ -5,12 +5,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "s3" {
-    bucket = "jenkins-project-backend-seryum"
-    key = "backend/tf-backend-jenkins.tfstate"
-    region = "us-east-1"
-    
-  }
 }
 
 provider "aws" {
